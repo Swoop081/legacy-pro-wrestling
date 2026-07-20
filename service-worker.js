5be33c51e98ed152be5dd5c2ef90db9e85a320f2
Swoop081 <justinbelot8@gmail.com>
2026-07-20 18:13:08 +1000

    v8.1.6

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-/* LEGACY Pro Wrestling Service Worker 8.1.2.6 */
-const APP_VERSION = '8.1.2.6-build-1.5-vertical-alignment';
+/* LEGACY Pro Wrestling Service Worker 8.1.2.7 */
+const APP_VERSION = '8.1.2.7-build-1.5-vertical-alignment';
 const CACHE_NAME = `lpw-${APP_VERSION}`;
 const CRITICAL_FILES = ['index.html','game.js','onboarding-wording-fix.js','data.js','styles.css','version.json','service-worker.js','update-manager.js','assets/config/imageManager.js'];
 
